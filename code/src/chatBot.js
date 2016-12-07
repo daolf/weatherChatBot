623c6513696763a6ed81ed784f627f5f19009182
Pierre De Wulf <pierredewulf31@gmail.com>
2016-12-07 20:28:22 +0100

    Improvement of style

---
+++ code/src/chatBot.js
@@ -0,0 +1,56 @@
+// @flow
+'use strict';
+
+var Wit = require('node-wit').Wit;
+var getWeather = require('../src/weatherRetriever.js').getWeather;
+
+const accessToken = (() => {
+  return process.env.WIT_ACCESS_TOKEN;
+})();
+
+// Quickstart example
+// See https://wit.ai/ar7hur/quickstart
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send (request, response) {
+    console.log('sending...', JSON.stringify(response));
+  },
+  getForecast ({context, entities}) {
+    var location = firstEntityValue(entities, 'location');
+    if (location) {
+      return getWeather(location).then((info) => {
+        var context = {
+          forecast: info
+        };
+        console.log(context.forecast);
+        return context;
+      }).catch((err) => {
+        console.log(err);
+      });
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+      return context;
+    }
+  }
+};
+
+const client = new Wit({accessToken, actions});
+
+var botFactory = () => {
+  return client;
+};
+
+module.exports = botFactory;
--- code/src/main.js
+++ code/src/main.js
@@ -15,7 +15,7 @@ app.post('/', function (req, res) {
   var text = req.body.text;
   var context = {};
   var msg = '';
-  botClient.runActions('id', text, context).then((data) => {
+  botClient.runActions('', text, context).then((data) => {
     // The following logic is already in the botClient
     // I did not manage to reuse it from the API.
     if (data.forecast) {
